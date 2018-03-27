1431e937be2ad128ab3f9a96956c3fae3f8b0e55
liubinbin <lotosbin@gmail.com>
2018-03-27 23:05:05 +0800

    feat(ios): init

---
+++ ios/RNRongCloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RNRongCloud
+//
+//  Created by 刘斌斌 on 27/03/2018.
+//  Copyright © 2018 lotos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
